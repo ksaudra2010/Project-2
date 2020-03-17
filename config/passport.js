99661118a849e90fb625b1b27909cf04bd883f83
Code Master <jeffbell.kc@gmail.com>
2020-03-16 22:39:45 -0500

    Added some base code

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var db = require('../models');
-=======
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-
-var db = require("../models");
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
+const db = require('../models');
 
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
   // Our user will sign in using an email, rather than a "username"
   {
-<<<<<<< HEAD
     usernameField: 'email',
-=======
-    usernameField: "email"
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
   },
   function(email, password, done) {
     // When a user tries to sign in this code runs
@@ -30,21 +19,13 @@ passport.use(new LocalStrategy(
       // If there's no user with the given email
       if (!dbUser) {
         return done(null, false, {
-<<<<<<< HEAD
           message: 'Incorrect email.'
-=======
-          message: "Incorrect email."
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
-<<<<<<< HEAD
           message: 'Incorrect password.'
-=======
-          message: "Incorrect password."
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
         });
       }
       // If none of the above, return the user
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Requiring necessary npm packages
-<<<<<<< HEAD
 const express = require('express');
 const session = require('express-session');
 // Requiring passport as we've configured it
@@ -16,29 +15,10 @@ app.use(express.json());
 app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-=======
-var express = require("express");
-var session = require("express-session");
-// Requiring passport as we've configured it
-var passport = require("./config/passport");
-
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
-
-// Creating express app and configuring middleware needed for authentication
-var app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-<<<<<<< HEAD
 require('./routes/html-routes.js')(app);
 require('./routes/api-routes.js')(app);
 
@@ -46,14 +26,5 @@ require('./routes/api-routes.js')(app);
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
-=======
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
->>>>>>> 18b5435854dfbb03908d942f4b6b21e01245ea9f
   });
 });
